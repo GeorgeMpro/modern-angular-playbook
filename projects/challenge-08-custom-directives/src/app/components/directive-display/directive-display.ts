fd7b5b203abf38503af8d5741429970fb0bfb6e0
George <43846095+GeorgeMpro@users.noreply.github.com>
2026-04-22 21:05:50 +0300

    done debounce click

--- projects/challenge-08-custom-directives/src/app/components/directive-display/directive-display.ts
+++ projects/challenge-08-custom-directives/src/app/components/directive-display/directive-display.ts
@@ -2,34 +2,48 @@ import {Component, signal} from '@angular/core';
 
 import {Highlight} from '../../directives/highlight';
 import {TitleCasePipe} from '@angular/common';
+import {DebounceClick} from '../../directives/debounce-click';
 
 interface Colors {
   label: string;
   color: string;
 }
 
+const COLORS: Record<string, string> = {
+  'green': 'lightgreen',
+  'yellow': '#fef08a',
+  'cyan': '#a5f3fc',
+  'rose': '#fecdd3',
+  'violet': '#ddd6fe',
+  'amber': '#fde68a',
+  'emerald': '#a7f3d0'
+}
+
 @Component({
   selector: 'app-directive-display',
-  imports: [Highlight, TitleCasePipe
+  imports: [Highlight, TitleCasePipe, DebounceClick
   ],
   templateUrl: './directive-display.html',
   styleUrl: './directive-display.scss',
 })
 export class DirectiveDisplay {
-  private readonly colors: Record<string, string> = {
-    'green': 'lightgreen',
-    'yellow': '#fef08a',
-    'cyan': '#a5f3fc',
-    'rose': '#fecdd3',
-    'violet': '#ddd6fe',
-    'amber': '#fde68a',
-    'emerald': '#a7f3d0'
-  }
-  protected readonly colorsDisplay: Colors[] = Object.entries(this.colors).map(
+  protected readonly clickCount = signal<number>(0);
+  protected readonly log = signal<string[]>([]);
+
+  protected readonly colorsDisplay: Colors[] = Object.entries(COLORS).map(
     ([label, color]) => ({
       label: label,
       color: color
     })
   );
+
   protected readonly color = signal<string>('');
+  protected onDebounceClick(): void {
+    this.clickCount.update(v => v + 1);
+    this.debounceLog();
+  }
+
+  protected debounceLog() {
+    this.log.update(l => [`Action triggered at ${new Date().toLocaleTimeString()}`, ...l].slice(0, 5));
+  }
 }
