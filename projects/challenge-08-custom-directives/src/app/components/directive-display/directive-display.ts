34e2525c9a9ea3c7db03a1662e0cad18fc009794
George <43846095+GeorgeMpro@users.noreply.github.com>
2026-04-23 08:44:38 +0300

    adding demo shell

--- projects/challenge-08-custom-directives/src/app/components/directive-display/directive-display.ts
+++ projects/challenge-08-custom-directives/src/app/components/directive-display/directive-display.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 
 import {HighlightDemo} from '../highlight-demo/highlight-demo';
 import {DebounceClickDemo} from '../debounce-click-demo/debounce-click-demo';
+import {InfiniteScrollDemo} from '../infinte-scroll-demo/infinite-scroll-demo.component';
 
 
 @Component({
   selector: 'app-directive-display',
-  imports: [HighlightDemo, DebounceClickDemo
+  imports: [HighlightDemo, DebounceClickDemo, InfiniteScrollDemo,
   ],
   templateUrl: './directive-display.html',
   styleUrl: './directive-display.scss',
---
+++ projects/challenge-08-custom-directives/src/app/components/infinte-scroll-demo/infinite-scroll-demo.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-infinte-scroll-demo',
+  imports: [],
+  templateUrl: './infinite-scroll-demo.component.html',
+  styleUrl: './infinite-scroll-demo.component.scss',
+})
+export class InfiniteScrollDemo {
+
+}
