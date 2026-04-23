b77f607cecf19ede61f39763dd51eeee06d54c8b
George <43846095+GeorgeMpro@users.noreply.github.com>
2026-04-23 08:05:39 +0300

    updated styles and added functionality to highlight directive

--- projects/challenge-08-custom-directives/src/app/components/directive-display/directive-display.ts
+++ projects/challenge-08-custom-directives/src/app/components/directive-display/directive-display.ts
@@ -3,25 +3,12 @@ import {Component, signal} from '@angular/core';
 import {Highlight} from '../../directives/highlight';
 import {TitleCasePipe} from '@angular/common';
 import {DebounceClick} from '../../directives/debounce-click';
+import {HighlightDemo} from '../highlight-demo/highlight-demo';
 
-interface Colors {
-  label: string;
-  color: string;
-}
-
-const COLORS: Record<string, string> = {
-  'green': 'lightgreen',
-  'yellow': '#fef08a',
-  'cyan': '#a5f3fc',
-  'rose': '#fecdd3',
-  'violet': '#ddd6fe',
-  'amber': '#fde68a',
-  'emerald': '#a7f3d0'
-}
 
 @Component({
   selector: 'app-directive-display',
-  imports: [Highlight, TitleCasePipe, DebounceClick
+  imports: [Highlight, TitleCasePipe, DebounceClick, HighlightDemo
   ],
   templateUrl: './directive-display.html',
   styleUrl: './directive-display.scss',
@@ -30,14 +17,6 @@ export class DirectiveDisplay {
   protected readonly clickCount = signal<number>(0);
   protected readonly log = signal<string[]>([]);
 
-  protected readonly colorsDisplay: Colors[] = Object.entries(COLORS).map(
-    ([label, color]) => ({
-      label: label,
-      color: color
-    })
-  );
-
-  protected readonly color = signal<string>('');
   protected onDebounceClick(): void {
     this.clickCount.update(v => v + 1);
     this.debounceLog();
---
+++ projects/challenge-08-custom-directives/src/app/components/highlight-demo/highlight-demo.ts
@@ -0,0 +1,39 @@
+import {Component, signal} from '@angular/core';
+import {Highlight} from '../../directives/highlight';
+import {TitleCasePipe} from '@angular/common';
+
+interface Colors {
+  label: string;
+  color: string;
+}
+
+const COLORS: Record<string, string> = {
+  'slate': '#1e293b',
+  'yellow': '#fef08a',
+  'cyan': '#a5f3fc',
+  'rose': '#fecdd3',
+  'violet': '#ddd6fe',
+  'emerald': '#a7f3d0',
+  'indigo': '#312e81',
+}
+
+@Component({
+  selector: 'app-highlight-demo',
+  imports: [
+    Highlight,
+    TitleCasePipe
+  ],
+  templateUrl: './highlight-demo.html',
+  styleUrl: './highlight-demo.scss',
+})
+export class HighlightDemo {
+
+  protected readonly colorsDisplay: Colors[] = Object.entries(COLORS).map(
+    ([label, color]) => ({
+      label: label,
+      color: color
+    })
+  );
+
+  protected readonly color = signal<string>('');
+}
